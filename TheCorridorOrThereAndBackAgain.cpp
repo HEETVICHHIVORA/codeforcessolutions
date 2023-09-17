4651b78252a0d3d7ea47d58ebf71f227812f0ad6
HEETVICHHIVORA <heetvic@gmail.com>
2023-09-17 16:36:28 +0530

    corridor

--- TheCorridorOrThereAndBackAgain.cpp
+++ TheCorridorOrThereAndBackAgain.cpp
@@ -1,21 +1,27 @@
 #include<iostream>
+#include<climits>
 
 using namespace std;
 int main (){
-    int n ;
+    int t ;
+    cin>>t;
+    while(t--){
+        int n ;
     cin>>n;
     int d[n];
     int s[n];
+    
     for(int i =0;i<n;i++){
         cin>>d[i]>>s[i];
     }
-    int j ;
-    int sec = 0 ;
-    int c =0;
-    for(j =0;j<n;j++){
-        for(int i=0;i<j;i++){
-
-        }
+    int k = INT_MAX;
+    int add [n];
+     for(int i =0;i<n;i++){
+       k =min((d[i]+((s[i]-1)/2)),k);
     }
+    
+    cout<<k;
 
+    }
+    
 }
